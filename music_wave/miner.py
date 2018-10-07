495de4aa6d3293a893f99de5b7940696d8c882c8
Alan Arteaga <alanarteagav@ciencias.unam.mx>
2018-10-07 17:49:00 -0500

    Template methods for miner and reader; getters and setters for rola file

---
+++ music_wave/miner.py
@@ -0,0 +1,5 @@
+class Miner :
+
+    def create_database(self):
+
+    def populate_database(self):
---
+++ music_wave/rola.py
@@ -0,0 +1,54 @@
+class Rola :
+
+    def get_id(self):
+
+    def get_performer(self):
+
+    def get_album(self):
+
+    def get_path(self):
+
+    def get_title(self):
+
+    def get_track(self):
+
+    def get_year(self):
+
+    def get_genre(self):
+
+    def set_id(self, id):
+
+    def set_performer(self, performer):
+
+    def set_album(self, album):
+
+    def set_path(self, path):
+
+    def set_title(self, title):
+
+    def set_track(self, track):
+
+    def set_year(self, year):
+
+    def set_genre(self, genre):
+
+class Rola_builder :
+
+    def __init__(self):
+        self.rola = Rola()
+
+    def with_id(self, id):
+
+    def with_performer(self, performer):
+
+    def with_album(self, album):
+
+    def with_path(self, path):
+
+    def with_title(self, title):
+
+    def with_track(self, track):
+
+    def with_year(self, year):
+
+    def with_genre(self, genre):
