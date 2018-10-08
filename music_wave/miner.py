190efb4f455f8c65b3f920e400469cb7ae5bc62d
Alan Arteaga <alanarteagav@ciencias.unam.mx>
2018-10-08 10:13:24 -0500

    Fixing constructors miner, reader

--- music_wave/miner.py
+++ music_wave/miner.py
@@ -1,10 +1,11 @@
 class Miner :
 
-    def __init__(self, path):
-        self.path = path
+    def __init__(self, directory, database_name):
+        self.directory = directory
+        self.database_name = database_name
 
     def create_database(self):
         pass
 
-    def populate_database(self):
+    def populate_database(self, path):
         pass
--- music_wave/reader.py
+++ music_wave/reader.py
@@ -1,7 +1,8 @@
 class Reader :
 
-    def __init__(self, path):
-        self.path = path
-
+    def __init__(self, directory, database_name):
+        self.directory = directory
+        self.database_name = database_name
+        
     def get_rolas_from_database(self):
         pass
