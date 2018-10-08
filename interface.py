45efcaa59da65d54d1a55fa56684aef37ea146ae
Alan Arteaga <alanarteagav@ciencias.unam.mx>
2018-10-07 23:56:29 -0500

    Editing interface and glade file

--- interface.py
+++ interface.py
@@ -1,8 +1,21 @@
+import sys
+
 import gi
 gi.require_version("Gtk", "3.0")
 from gi.repository import Gtk
 
-window = Gtk.Window(title="Hello World")
-window.show()
-window.connect("destroy", Gtk.main_quit)
+class Handler():
+    def quit(self, *args):
+        Gtk.main_quit()
+
+    def mineMusic(self, *args):
+        Gtk.main_quit()
+
+builder = Gtk.Builder()
+builder.add_from_file("resources/main.glade")
+builder.connect_signals(Handler())
+
+window = builder.get_object("main_window")
+window.show_all()
+
 Gtk.main()
