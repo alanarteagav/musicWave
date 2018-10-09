9619574b1226927e3b6f7fafb772ea568f5c3e68
Alan Arteaga <alanarteagav@ciencias.unam.mx>
2018-10-09 12:38:34 -0500

    Removing reader and test_reader file.

--- music_wave/reader.py
+++
@@ -1,8 +0,0 @@
-class Reader :
-
-    def __init__(self, directory, database_name):
-        self.directory = directory
-        self.database_name = database_name
-        
-    def get_rolas_from_database(self):
-        pass
