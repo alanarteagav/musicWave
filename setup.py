15b98941fdf292a35149e4d424e6c1451abf57dc
Alan Arteaga <alanarteagav@ciencias.unam.mx>
2018-10-15 21:31:55 -0500

    Updating setup.py file to make it compatible with the test suite.

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+
+setup(
+   name='musicWave',
+   version='1.1',
+   description='System to create and manipulate a musical database.',
+   author='Alan Arteaga',
+   author_email='alanarteagav@ciencias.unam.mx',
+   test_suite="tests"
+)
