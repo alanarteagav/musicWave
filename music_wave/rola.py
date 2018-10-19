48adc862396c6660ca360eb7d6c604454efeecc2
Alan Arteaga <alanarteagav@ciencias.unam.mx>
2018-10-19 15:42:14 -0500

    Comments for rola class.

--- music_wave/rola.py
+++ music_wave/rola.py
@@ -3,7 +3,26 @@ class Rola :
        a database."""
 
     def __init__(self, id, performer_id, album_id, path, title, track, year, genre):
-        """Defines a new rola"""
+        """Defines a new rola.
+
+            Parameters:
+
+            id (int) : the identifier for the rola.
+
+            performer_id (int) : the identifier for the performer of the rola.
+
+            album_id (int) : the identifier for the album of the rola.
+
+            path (str) : the path of the mp3 file.
+
+            title (str) : the title of the rola.
+
+            track (int) : the track number of the rola.
+
+            year (int) : the year of the rola.
+
+            genre (str) : the genre of the rola.
+            """
         self.id = id
         self.performer_id = performer_id
         self.album_id = album_id
@@ -14,65 +33,68 @@ class Rola :
         self.genre = genre
 
     def get_id(self):
-        """Returns the rola id"""
+        """Returns : the rola id (int)"""
         return self.id
 
     def get_performer_id(self):
-        """Returns the rola performer id"""
+        """Returns : the rola performer id (int)"""
         return self.performer_id
 
     def get_album_id(self):
-        """Returns the rola album id"""
+        """Returns : the rola album id (int)"""
         return self.album_id
 
     def get_path(self):
-        """Returns the path of the rola"""
+        """Returns : the path of the rola (str)"""
         return self.path
 
     def get_title(self):
-        """Returns the title of the rola"""
+        """Returns : the title of the rola (str)"""
         return self.title
 
     def get_track(self):
-        """Returns the rola track number"""
+        """Returns : the rola track number (int)"""
         return self.track
 
     def get_year(self):
-        """Returns the year of the rola"""
+        """Returns : the year of the rola (int)"""
         return self.year
 
     def get_genre(self):
-        """Returns the genre of the rola"""
+        """Returns : the genre of the rola (str)"""
         return self.genre
 
     def set_id(self, id):
-        """Sets a new rola id"""
+        """Sets a new rola id
+
+            Parameter : id (str) : the identifier for the rola."""
         self.id = id
 
     def set_performer_id(self, performer_id):
-        """Sets a new rola performer id"""
+        """Parameter : performer_id (int) : the identifier of the
+           rola performer"""
         self.performer_id = performer_id
 
     def set_album_id(self, album_id):
-        """Sets a new rola performer id"""
+        """Parameter : album_id (int) : the indentifier of the rola's album."""
         self.album_id = album_id
 
     def set_path(self, path):
-        """Sets a new path for the rola"""
+        """Parameter : path (str) : the path of the mp3 file."""
         self.path = path
 
     def set_title(self, title):
-        """Sets a new title for the rola"""
+        """Parameter : title (str) : the title of the rola."""
         self.title = title
 
     def set_track(self, track):
-        """Sets a new track number for the rola"""
+        """Parameter : track (int) : the track number of the rola."""
         self.track = track
 
     def set_year(self, year):
-        """Sets a new year for the rola"""
+        """Parameter : year (int) : the year of the rola."""
         self.year = year
 
     def set_genre(self, genre):
-        """Sets a new genre for the rola"""
+        """Parameter : genre (int) : the genre of the rola."""
         self.genre = genre
